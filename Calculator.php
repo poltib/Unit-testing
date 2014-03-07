35c52e72e4e229e52c174b627ac632a0e68d2fd1
Jérémy Thiry <jeremy@tagtagcity.com>
2014-03-07 09:12:16 +0100

    Indentation to spaces

--- Calculator.php
+++ Calculator.php
@@ -2,24 +2,24 @@
 
 class Calculator
 {
-	/**
+     /**
      * The start value.
      *
      * @var null
      */
-	protected $result = null;
+     protected $result = null;
 
-	/**
+     /**
      * Get the result of the operation
      *
      * @return int
      */
-	public function getResult()
-	{
-		return $this->result;
-	}
+     public function getResult()
+     {
+          return $this->result;
+     }
 
-	/**
+     /**
      * Add the param(s) to the result
      * We just foreach throught the function's parameters. Into this loop we check if
      * the parameters are numeric. If it is we add the parameter to the result
@@ -27,13 +27,13 @@ class Calculator
      *
      *  @param  int
      */
-	// the method will be here
+     // the method will be here
 
-	/**
+     /**
      * Substract the param(s) to the result
      * Same as the addition but we substract.
      *
      *  @param  int
      */
-	// the method will be here
+     // the method will be here
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -3,55 +3,55 @@ use \Mockery as m;
 
 class CalculatorTest extends PHPUnit_Framework_TestCase
 {
-	public function setUp()
-	{
-		$this->calc = new Calculator;
-	}
+    public function setUp()
+    {
+        $this->calc = new Calculator;
+    }
 
-	public function testResultDefaultToNull()
-	{
-		$this->assertNull($this->calc->getResult());
-	}
+    public function testResultDefaultToNull()
+    {
+        $this->assertNull($this->calc->getResult());
+    }
 
-	/**
-	* Here we test the future Add method that will add numbers to the result.
-	* This test assert that if we call this method with the integer 5 it will return * the result (null at the begining) plus 5.
-	*/
-	public function testAddNumbers()
-	{
-		$this->calc->add(5);
+    /**
+    * Here we test the future Add method that will add numbers to the result.
+    * This test assert that if we call this method with the integer 5 it will return * the result (null at the begining) plus 5.
+    */
+    public function testAddNumbers()
+    {
+        $this->calc->add(5);
 
-		$this->assertEquals(5, $this->calc->getResult());
-	}
+        $this->assertEquals(5, $this->calc->getResult());
+    }
 
-	/**
-	* @expectedException InvalidArgumentException
-	*/
-	public function testRequiresNumericValue()
-	{
-		// We check that the parameters are numeric
-		$this->calc->add('five');
-	}
+    /**
+    * @expectedException InvalidArgumentException
+    */
+    public function testRequiresNumericValue()
+    {
+        // We check that the parameters are numeric
+        $this->calc->add('five');
+    }
 
-	/**
-	* In this test we check that the functions accepts multiple arguments.
-	* So if we call add with (1,2,3,4) it should return 10.
-	*/
-	public function testAcceptsMultipleArgs()
-	{
-		$this->calc->add(1,2,3,4);
+    /**
+    * In this test we check that the functions accepts multiple arguments.
+    * So if we call add with (1,2,3,4) it should return 10.
+    */
+    public function testAcceptsMultipleArgs()
+    {
+        $this->calc->add(1,2,3,4);
 
-		$this->assertEquals(10, $this->calc->getResult());
-		$this->assertNotEquals('plop', $this->calc->getResult());
-	}
+        $this->assertEquals(10, $this->calc->getResult());
+        $this->assertNotEquals('plop', $this->calc->getResult());
+    }
 
-	/**
-	* Same tests that for the addition but for the substraction.
-	*/
-	public function testSubstract()
-	{
-		$this->calc->substract(4);
+    /**
+    * Same tests that for the addition but for the substraction.
+    */
+    public function testSubstract()
+    {
+        $this->calc->substract(4);
 
-		$this->assertEquals(-4, $this->calc->getResult());
-	}	
+        $this->assertEquals(-4, $this->calc->getResult());
+    }   
 }
